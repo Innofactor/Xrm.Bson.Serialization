0c8cb82d86d36cdc6df5626c5dca381ef343d6b7
Alexey Shytikov <alexey.shytikov@cinteros.se>
2017-04-19 07:37:48 +0200

    Adding simple implementation of MoneySerializer

---
+++ Innofactor.Xrm.Bson/MoneySerializer.cs
@@ -0,0 +1,20 @@
+﻿namespace Innofactor.Xrm.Bson
+{
+    using System;
+    using MongoDB.Bson.Serialization;
+
+    internal class MoneySerializer : IBsonSerializer
+    {
+        public Type ValueType => throw new NotImplementedException();
+
+        public object Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Serialize(BsonSerializationContext context, BsonSerializationArgs args, object value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
