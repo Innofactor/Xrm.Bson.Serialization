01a1d397838c0615076c0105d8d41a017f22383f
Alexey Shytikov <alexey.shytikov@cinteros.se>
2017-04-19 08:04:10 +0200

    Adding failing tests for attributes.

---
+++ Innofactor.Xrm.Bson.Tests/AttributeTests.cs
@@ -0,0 +1,164 @@
+﻿namespace Innofactor.Xrm.Bson.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Microsoft.Xrm.Sdk;
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization;
+
+    [TestClass]
+    public class AttributeTests
+    {
+        #region Public Methods
+
+        [TestMethod, TestCategory("Bson Tests")]
+        public void DeSerializeEntityDateTime_Success()
+        {
+            // Arrange
+            var expectedValue = DateTime.Now;
+            var expectedEntity = new Entity("account");
+            expectedEntity.Attributes.Add("key", expectedValue);
+            var bson = expectedEntity.ToBsonDocument();
+
+            // Act
+            var actualEntity = BsonSerializer.Deserialize<Entity>(bson);
+            var actualValue = (DateTime)actualEntity.Attributes["key"];
+
+            // Assert
+            Assert.AreEqual(typeof(DateTime), actualValue.GetType());
+            Assert.AreEqual(expectedValue.ToShortTimeString(), actualValue.ToShortTimeString());
+        }
+
+        [TestMethod, TestCategory("Bson Tests")]
+        public void DeSerializeEntityBoolean_Success()
+        {
+            // Arrange
+            var expectedValue = true;
+            var expectedEntity = new Entity("account");
+            expectedEntity.Attributes.Add("key", expectedValue);
+            var bson = expectedEntity.ToBsonDocument();
+
+            // Act
+            var actualEntity = BsonSerializer.Deserialize<Entity>(bson);
+            var actualValue = actualEntity.Attributes["key"];
+
+            // Assert
+            Assert.AreEqual(typeof(bool), actualValue.GetType());
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod, TestCategory("Bson Tests")]
+        public void DeSerializeEntityInteger_Success()
+        {
+            // Arrange
+            var expectedValue = 1;
+            var expectedEntity = new Entity("account");
+            expectedEntity.Attributes.Add("key", expectedValue);
+            var bson = expectedEntity.ToBsonDocument();
+
+            // Act
+            var actualEntity = BsonSerializer.Deserialize<Entity>(bson);
+            var actualValue = actualEntity.Attributes["key"];
+
+            // Assert
+            Assert.AreEqual(typeof(int), actualValue.GetType());
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod, TestCategory("Bson Tests")]
+        public void DeSerializeEntityString_Success()
+        {
+            // Arrange
+            var expectedValue = "value";
+            var expectedEntity = new Entity("account");
+            expectedEntity.Attributes.Add("key", expectedValue);
+            var bson = expectedEntity.ToBsonDocument();
+
+            // Act
+            var actualEntity = BsonSerializer.Deserialize<Entity>(bson);
+            var actualValue = actualEntity.Attributes["key"];
+
+            // Assert
+            Assert.AreEqual(typeof(string), actualValue.GetType());
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [TestMethod, TestCategory("Bson Tests")]
+        public void SerializeEntityDateTime_Success()
+        {
+            // Arrange
+            var expectedValue = DateTime.Now;
+            var entity = new Entity("account");
+            entity.Attributes.Add("key", expectedValue);
+
+            // Act
+            var bson = entity.ToBsonDocument();
+            var actualValue = bson["key"];
+
+            // Assert
+            Assert.AreEqual(true, actualValue.IsValidDateTime);
+            Assert.AreEqual(expectedValue.ToShortTimeString(), actualValue.ToUniversalTime().ToShortTimeString());
+        }
+
+        [TestMethod, TestCategory("Bson Tests")]
+        public void SerializeEntityBoolean_Success()
+        {
+            // Arrange
+            var expectedValue = true;
+            var entity = new Entity("account");
+            entity.Attributes.Add("key", expectedValue);
+
+            // Act
+            var bson = entity.ToBsonDocument();
+            var actualValue = bson["key"];
+
+            // Assert
+            Assert.AreEqual(true, actualValue.IsBoolean);
+            Assert.AreEqual(expectedValue, actualValue.AsBoolean);
+        }
+
+        [TestMethod, TestCategory("Bson Tests")]
+        public void SerializeEntityInteger_Success()
+        {
+            // Arrange
+            var expectedValue = 1;
+            var entity = new Entity("account");
+            entity.Attributes.Add("key", expectedValue);
+
+            // Act
+            var bson = entity.ToBsonDocument();
+            var actualValue = bson["key"];
+
+            // Assert
+            Assert.AreEqual(true, actualValue.IsInt32);
+            Assert.AreEqual(expectedValue, actualValue.AsInt32);
+        }
+
+        [TestMethod, TestCategory("Bson Tests")]
+        public void SerializeEntityString_Success()
+        {
+            // Arrange
+            var expectedValue = "value";
+            var entity = new Entity("account");
+            entity.Attributes.Add("key", expectedValue);
+
+            // Act
+            var bson = entity.ToBsonDocument();
+            var actualValue = bson["key"];
+
+            // Assert
+            Assert.AreEqual(true, actualValue.IsString);
+            Assert.AreEqual(expectedValue, actualValue.AsString);
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            // Setting up serializers
+            BsonSerializer.RegisterSerializationProvider(new SerializationPrivider());
+        }
+
+        #endregion Public Methods
+    }
+
+}
