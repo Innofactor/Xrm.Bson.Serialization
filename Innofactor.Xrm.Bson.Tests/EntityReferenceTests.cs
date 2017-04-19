1ac9f9e65cf4b94a938404c2b3782e45f621d051
Alexey Shytikov <alexey.shytikov@cinteros.se>
2017-04-19 07:56:45 +0200

    Adding failing entity reference tests

---
+++ Innofactor.Xrm.Bson.Tests/EntityReferenceTests.cs
@@ -0,0 +1,64 @@
+﻿namespace Innofactor.Xrm.Bson.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Microsoft.Xrm.Sdk;
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization;
+
+    [TestClass]
+    public class EntityReferenceTests
+    {
+        #region Public Methods
+
+        [TestMethod, TestCategory("Bson Tests")]
+        public void DeSerializeEntityEntityReference_Success()
+        {
+            // Arrange
+            var expectedValue = new EntityReference("contact", Guid.NewGuid());
+            var expectedEntity = new Entity("account");
+            expectedEntity.Attributes.Add("key", expectedValue);
+            var bson = expectedEntity.ToBsonDocument();
+
+            // Act
+            var actualEntity = BsonSerializer.Deserialize<Entity>(bson);
+            var actualValue = actualEntity.Attributes["key"];
+
+            // Assert
+            Assert.AreEqual(typeof(EntityReference), actualValue.GetType());
+            Assert.AreEqual(expectedValue.LogicalName, (actualValue as EntityReference).LogicalName);
+            Assert.AreEqual(expectedValue.Id, (actualValue as EntityReference).Id);
+        }
+
+        [TestMethod, TestCategory("Bson Tests")]
+        public void SerializeEntityEntityReference_Success()
+        {
+            // Arrange
+            var expectedValue = new EntityReference("contact", Guid.NewGuid());
+            var entity = new Entity("account");
+            entity.Attributes.Add("key", expectedValue);
+
+            // Act
+            var bson = entity.ToBsonDocument();
+            var actualValue = bson["key"];
+
+            // Assert
+            Assert.AreEqual(true, actualValue.IsBsonDocument);
+
+            // Checking logical name
+            Assert.AreEqual(expectedValue.LogicalName, actualValue.AsBsonDocument["_name"].AsString);
+
+            // Checking id
+            Assert.AreEqual(expectedValue.Id, actualValue.AsBsonDocument["_id"].AsGuid);
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            // Setting up serializers
+            BsonSerializer.RegisterSerializationProvider(new SerializationPrivider());
+        }
+
+        #endregion Public Methods
+    }
+}
