72fa5a7e65ffb7c66e8883af3f5d0175bfb4d2ab
Alexey Shytikov <alexey.shytikov@cinteros.se>
2017-04-19 07:54:07 +0200

    Make all entity serialization tests pass

--- Innofactor.Xrm.Bson/EntitySerializer.cs
+++ Innofactor.Xrm.Bson/EntitySerializer.cs
@@ -2,23 +2,50 @@
 {
     using Innofactor.Xrm.Bson.Extensions;
     using Microsoft.Xrm.Sdk;
+    using MongoDB.Bson;
+    using MongoDB.Bson.IO;
     using MongoDB.Bson.Serialization;
     using System;
 
-    internal class EntitySerializer : IBsonSerializer
+    internal class EntitySerializer : IBsonSerializer<Entity>
     {
         public Type ValueType => typeof(Entity);
 
         public object Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
         {
-            throw new NotImplementedException();
+            return Deserialize(context, args);
         }
 
         public void Serialize(BsonSerializationContext context, BsonSerializationArgs args, object value)
         {
+            Serialize(context, args, value as Entity);
+        }
+
+        public void Serialize(BsonSerializationContext context, BsonSerializationArgs args, Entity value)
+        {
             context.Writer.WriteStartDocument();
-            context.Writer.WriteReference((value as Entity).ToEntityReference());
+            context.Writer.WriteReference(value.ToEntityReference());
             context.Writer.WriteEndDocument();
+        }
+
+        Entity IBsonSerializer<Entity>.Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        {
+            context.Reader.ReadStartDocument();
+
+            var entity = new Entity(context.Reader.ReadString(), new Guid(context.Reader.ReadBinaryData().AsByteArray));
+
+            while (context.Reader.State != BsonReaderState.Done && context.Reader.State != BsonReaderState.Initial)
+            {
+                var type = context.Reader.ReadBsonType();
+                switch (type)
+                {
+                    case BsonType.EndOfDocument:
+                        context.Reader.ReadEndDocument();
+                        break;
+                }
+            }
+
+            return entity;
         }
     }
 }
