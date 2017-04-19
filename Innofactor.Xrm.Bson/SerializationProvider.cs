ee997ebbc16ca4aee612924c2d3398be80e1359e
Alexey Shytikov <alexey.shytikov@cinteros.se>
2017-04-19 07:34:52 +0200

    Adding wireframe implementation of serialization provider

---
+++ Innofactor.Xrm.Bson/SerializationProvider.cs
@@ -0,0 +1,35 @@
+﻿namespace Innofactor.Xrm.Bson
+{
+    using System;
+    using Microsoft.Xrm.Sdk;
+    using MongoDB.Bson.Serialization;
+
+    public class SerializationPrivider : IBsonSerializationProvider
+    {
+        #region Public Methods
+
+        public IBsonSerializer GetSerializer(Type type)
+        {
+            if (type == typeof(Entity))
+            {
+                return new EntitySerializer();
+            }
+            else if (type == typeof(EntityReference))
+            {
+                return new EntityReferenceSerializer();
+            }
+            else if (type == typeof(OptionSetValue))
+            {
+                return new OptionSetValueSerializer();
+            }
+            else if (type == typeof(Money))
+            {
+                return new MoneySerializer();
+            }
+
+            return default(IBsonSerializer);
+        }
+
+        #endregion Public Methods
+    }
+}
