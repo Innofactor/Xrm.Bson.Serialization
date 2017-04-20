55a942b2eb3d8885be973a15780d6d00b28377cd
Alexey Shytikov <alexey.shytikov@cinteros.se>
2017-04-20 07:37:03 +0200

    Adding failing OptionSetValue tests

---
+++ Innofactor.Xrm.Bson.Tests/OptionSetValueTests.cs
@@ -0,0 +1,58 @@
+﻿namespace Innofactor.Xrm.Bson.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Microsoft.Xrm.Sdk;
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization;
+
+    [TestClass]
+    public class OptionSetValueTests
+    {
+        #region Public Methods
+
+        [TestMethod, TestCategory("Bson Tests")]
+        public void DeSerializeEntityOptionSetValue_Success()
+        {
+            // Arrange
+            var expectedValue = 1;
+            var expectedEntity = new Entity("account");
+            expectedEntity.Attributes.Add("key", new OptionSetValue(expectedValue));
+            var bson = expectedEntity.ToBsonDocument();
+
+            // Act
+            var actualEntity = BsonSerializer.Deserialize<Entity>(bson);
+            var actualValue = actualEntity.Attributes["key"];
+
+            // Assert
+            Assert.AreEqual(typeof(OptionSetValue), actualValue.GetType());
+            Assert.AreEqual(expectedValue, (actualValue as OptionSetValue).Value);
+        }
+
+        [TestMethod, TestCategory("Bson Tests")]
+        public void SerializeEntityOptionSet_Success()
+        {
+            // Arrange
+            var expectedValue = 1;
+            var entity = new Entity("account");
+            entity.Attributes.Add("key", new OptionSetValue(expectedValue));
+
+            // Act
+            var bson = entity.ToBsonDocument();
+            var actualValue = bson["key"];
+
+            // Assert
+            Assert.AreEqual(true, actualValue.IsBsonDocument);
+            Assert.AreEqual(expectedValue, actualValue.AsBsonDocument["_option"].AsInt32);
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            // Setting up serializers
+            BsonSerializer.RegisterSerializationProvider(new SerializationPrivider());
+        }
+
+        #endregion Public Methods
+    }
+
+}
